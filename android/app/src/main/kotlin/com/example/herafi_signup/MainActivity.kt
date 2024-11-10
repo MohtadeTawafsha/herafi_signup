0b93e0f032b0373d879c8e9606df7aa735741956
MohtadeTawafsha <mohtadedrabi@gmail.com>
2024-11-10 12:13:55 +0200

    first commit

--- android/app/src/main/kotlin/com/example/herafi_signup/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/herafi_signup/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.herafi_signup
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()

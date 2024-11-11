89d5dd731ee3f9afa20774ce5ec2e0cef949f84b
MohtadeTawafsha <mohtadedrabi@gmail.com>
2024-11-11 11:26:02 +0200

    Second  commit

--- lib/pages/register_craftsman.dart
+++ lib/pages/register_craftsman.dart
@@ -1,18 +1,213 @@
 import 'package:flutter/material.dart';
 
-class RegisterCraftsman extends StatelessWidget {
+import '../custome/custome_button.dart';
+
+class RegisterCraftsman extends StatefulWidget {
   const RegisterCraftsman({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterCraftsman> createState() => _RegisterCraftsmanState();
+}
+
+class _RegisterCraftsmanState extends State<RegisterCraftsman> {
+  final _formKey = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _dobController = TextEditingController();
+  final _addressController = TextEditingController();
+
+  final List<String> _categories = ['السباكة', 'النجارة', 'الكهرباء', 'الطلاء', 'أخرى'];
+  String? _selectedCategory;
+  int _yearsOfExperience = 0; // Starting at 0 years
+
+  void _incrementExperience() {
+    setState(() {
+      if (_yearsOfExperience < 60) {
+        _yearsOfExperience++;
+      }
+    });
+  }
+
+  void _decrementExperience() {
+    setState(() {
+      if (_yearsOfExperience > 0) {
+        _yearsOfExperience--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('تسجيل حساب الحرفي'),backgroundColor: Colors.blue),
-      body: Center(
-        child: const Text(
-          'صفحة تسجيل حساب الحرفي',
-          style: TextStyle(fontSize: 20),
+      appBar: AppBar(
+        title: const Text('تسجيل حساب الحرفي'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              // First Name
+              TextFormField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(labelText: 'الاسم الأول'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'يرجى إدخال الاسم الأول';
+                  }
+                  return null;
+                },
+              ),
+
+              // Last Name
+              TextFormField(
+                controller: _lastNameController,
+                decoration: const InputDecoration(labelText: 'الاسم الأخير'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'يرجى إدخال الاسم الأخير';
+                  }
+                  return null;
+                },
+              ),
+
+              // Phone Number
+              TextFormField(
+                controller: _phoneController,
+                decoration: const InputDecoration(labelText: 'رقم الهاتف'),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'يرجى إدخال رقم الهاتف';
+                  }
+                  return null;
+                },
+              ),
+
+              // Date of Birth
+              TextFormField(
+                controller: _dobController,
+                decoration: const InputDecoration(labelText: 'تاريخ الميلاد'),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime(2100),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      _dobController.text =
+                      "${pickedDate.toLocal()}".split(' ')[0];
+                    });
+                  }
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'يرجى إدخال تاريخ الميلاد';
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 10),
+
+              // Years of Experience with Increment/Decrement Buttons
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: _decrementExperience,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'سنوات الخبرة',
+                        border: OutlineInputBorder(),
+                      ),
+                      readOnly: true,
+                      textAlign: TextAlign.center,
+                      controller: TextEditingController(
+                        text: _yearsOfExperience.toString(),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: _incrementExperience,
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 10),
+
+              // Category Dropdown
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(labelText: 'التخصص'),
+                value: _selectedCategory,
+                items: _categories.map((String category) {
+                  return DropdownMenuItem<String>(
+                    value: category,
+                    child: Text(category),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedCategory = newValue;
+                  });
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'يرجى اختيار التخصص';
+                  }
+                  return null;
+                },
+              ),
+
+              // Address
+              TextFormField(
+                controller: _addressController,
+                decoration: const InputDecoration(labelText: 'العنوان'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'يرجى إدخال العنوان';
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 20),
+
+              // Submit Button
+              CustomButton(
+                icon: Icons.check,
+                label: 'تسجيل',
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Add your submit functionality here
+                    print("تم إرسال النموذج بنجاح! سنوات الخبرة: $_yearsOfExperience");
+                  }
+                },
+                color: Colors.blue,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _phoneController.dispose();
+    _dobController.dispose();
+    _addressController.dispose();
+    super.dispose();
   }
 }
--- lib/pages/register_customer.dart
+++ lib/pages/register_customer.dart
@@ -1,17 +1,166 @@
 import 'package:flutter/material.dart';
 
-class RegisterCustomer extends StatelessWidget {
+class RegisterCustomer extends StatefulWidget {
   const RegisterCustomer({Key? key}) : super(key: key);
 
   @override
+  State<RegisterCustomer> createState() => _RegisterCustomerState();
+}
+
+class _RegisterCustomerState extends State<RegisterCustomer> {
+  final _formKey = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _dobController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('تسجيل حساب العميل'),backgroundColor: Colors.blue),
-      body: Center(
-        child: const Text(
-          'صفحة تسجيل حساب العميل',
-          style: TextStyle(fontSize: 20),
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('تسجيل حساب العميل'),
+          backgroundColor: Colors.blue,
         ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                // First Name
+                TextFormField(
+                  controller: _firstNameController,
+                  decoration: const InputDecoration(labelText: 'الاسم الأول'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'يرجى إدخال الاسم الأول';
+                    }
+                    return null;
+                  },
+                ),
+
+                // Last Name
+                TextFormField(
+                  controller: _lastNameController,
+                  decoration: const InputDecoration(labelText: 'الاسم الأخير'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'يرجى إدخال الاسم الأخير';
+                    }
+                    return null;
+                  },
+                ),
+
+                // Phone Number
+                TextFormField(
+                  controller: _phoneController,
+                  decoration: const InputDecoration(labelText: 'رقم الهاتف'),
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'يرجى إدخال رقم الهاتف';
+                    }
+                    return null;
+                  },
+                ),
+
+                // Date of Birth
+                TextFormField(
+                  controller: _dobController,
+                  decoration: const InputDecoration(labelText: 'تاريخ الميلاد'),
+                  readOnly: true,
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime(2100),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        _dobController.text =
+                        "${pickedDate.toLocal()}".split(' ')[0];
+                      });
+                    }
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'يرجى إدخال تاريخ الميلاد';
+                    }
+                    return null;
+                  },
+                ),
+
+                const SizedBox(height: 20),
+
+                // Submit Button
+                CustomButton(
+                  icon: Icons.check,
+                  label: 'تسجيل',
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Add your submit functionality here
+                      print("تم إرسال النموذج بنجاح!");
+                    }
+                  },
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _phoneController.dispose();
+    _dobController.dispose();
+    super.dispose();
+  }
+}
+
+// Custom Button Widget
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+  final Color color;
+
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.onPressed,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, color: Colors.white),
+          const SizedBox(width: 8),
+          Text(
+            label,
+            style: const TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
